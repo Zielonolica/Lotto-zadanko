bf9f99edaab8ccc8afc898422cd23d5fc180582a
Zielonolica <jarzebczyk.kamila@gmail.com>
2018-04-20 23:31:29 +0200

    zrobione: wybieranie liczb przez usera + stworzenie z nich listy oraz wszystkie trzy warunki (do dopracowania (?))

--- lotto/venv/funkcje.py
+++ lotto/venv/funkcje.py
@@ -1,1 +1,38 @@
-print("hello kamila!")
+import random
+import time
+
+random.seed(time.time())
+
+def convertIntoInt(otrzymaneLiczby):
+    listaLiczbZnaki = otrzymaneLiczby.split(' ')
+    liczby = []
+    for i in listaLiczbZnaki:
+        liczby.append(int(i))
+    liczby.sort()
+    return liczby
+
+def ileLiczb(liczby):
+    n = 0
+    for i in liczby:
+        n += 1
+    return n
+
+def czyRozne(liczby):
+    for i in range(0,5):
+        for j in range(i+1,6):
+            if i != j and liczby[j] == liczby[i]:
+                return False
+    return True
+
+def sprZakres(liczby):
+    checker = 0
+    for i in range(0,6):
+        if liczby[i] < 1:
+            print("Liczba {0} jest poniżej dopuszczalnego zakresu!".format(liczby[i]))
+            checker += 1
+        elif liczby[i] > 49:
+            print("Liczba {0} jest powyżej dopuszczalnego zakresu (1-49)!".format(liczby[i]))
+            checker += 1
+    if checker > 0:
+        return False
+    return True
--- lotto/venv/main.py
+++ lotto/venv/main.py
@@ -1,2 +1,28 @@
 import funkcje
-print("hello world!")
+
+game = True
+
+while (True):
+  otrzymaneLiczby = input("Wybierz 6 różnych liczb z zakresu od 1 do 49 oddzielając je spacjami:\n")
+
+  #zamiana stringa na liste intów
+  liczby = funkcje.convertIntoInt(otrzymaneLiczby)
+
+  #czy podano 6 liczb
+  if funkcje.ileLiczb(liczby) > 6:
+    print("Podano za dużo liczb!")
+    continue
+  elif funkcje.ileLiczb(liczby) < 6:
+    print("Podano za mało liczb!")
+    continue
+
+  #czy podane liczby się nie powtarzają
+  if funkcje.czyRozne(liczby) == False:
+      print("Podane liczby się powtarzają!")
+      continue
+
+  #czy wszystkie licizby są od 1 do 49
+  if funkcje.sprZakres(liczby) == False:
+      continue
+
+  print("wszystko ok")
